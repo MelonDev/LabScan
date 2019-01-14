cad47837525ac8b7fae9ca8d58566550e64ab06a
android <59022222@live.up.ac.th>
2019-01-14 12:57:18 +0700

    Initial Commit

---
+++ LabScan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LabScan
+//
+//  Created by Android on 14/1/2562 BE.
+//  Copyright © 2562 Android. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ LabScanTests/LabScanTests.swift
@@ -0,0 +1,34 @@
+//
+//  LabScanTests.swift
+//  LabScanTests
+//
+//  Created by Android on 14/1/2562 BE.
+//  Copyright © 2562 Android. All rights reserved.
+//
+
+import XCTest
+@testable import LabScan
+
+class LabScanTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
